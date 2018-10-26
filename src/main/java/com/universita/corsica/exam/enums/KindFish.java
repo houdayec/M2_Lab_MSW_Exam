40c904e8cef5d159c055ce3203dee7008e97e94a
houdayec <houdayer.corentin@gmail.com>
2018-10-26 09:11:22 +0200

    Fish model added

---
+++ src/main/java/com/universita/corsica/exam/enums/KindFish.java
@@ -0,0 +1,10 @@
+package com.universita.corsica.exam.enums;
+
+/**
+ * Class that enumerates all kinds of fish
+ */
+public enum KindFish {
+
+    SEA_BASS, SEA_BREAM, SEA_SHARP, SEA_PLUS_PLUS
+
+}
---
+++ src/main/java/com/universita/corsica/exam/model/Fish.java
@@ -0,0 +1,117 @@
+package com.universita.corsica.exam.model;
+
+import com.universita.corsica.exam.enums.KindFish;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.core.geo.GeoPoint;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Class that describes a fish
+ */
+@Document(indexName = "fish", type="fish")
+public class Fish {
+
+    /**
+     * INTERN STATE
+     */
+    @NotNull
+    @Id
+    public String id;
+    public KindFish kindFish;
+    public int ageInMonths;
+    public GeoPoint position;
+    public boolean isProtected;
+
+    /**
+     * DEFAULT CONSTRUCTOR
+     */
+    public Fish() {
+    }
+
+    /**
+     * GETTERS AND SETTERS
+     */
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public KindFish getKindFish() {
+        return kindFish;
+    }
+
+    public void setKindFish(KindFish kindFish) {
+        this.kindFish = kindFish;
+    }
+
+    public int getAgeInMonths() {
+        return ageInMonths;
+    }
+
+    public void setAgeInMonths(int ageInMonths) {
+        this.ageInMonths = ageInMonths;
+    }
+
+    public GeoPoint getPosition() {
+        return position;
+    }
+
+    public void setPosition(GeoPoint position) {
+        this.position = position;
+    }
+
+    public boolean isProtected() {
+        return isProtected;
+    }
+
+    public void setProtected(boolean aProtected) {
+        isProtected = aProtected;
+    }
+
+    /**
+     * FLUENT API
+     */
+    public Fish withId(String id){
+        this.id = id;
+        return this;
+    }
+
+    public Fish withKind(KindFish kindFish){
+        this.kindFish = kindFish;
+        return this;
+    }
+
+    public Fish withAge(int ageInMonths){
+        this.ageInMonths = ageInMonths;
+        return this;
+    }
+
+    public Fish withPosition(GeoPoint position){
+        this.position = position;
+        return this;
+    }
+
+    public Fish isProtected(boolean isProtected){
+        this.isProtected = isProtected;
+        return this;
+    }
+
+    /**
+     * OVERRIDED METHODS
+     */
+    @Override
+    public String toString() {
+        return "Fish{" +
+                "id='" + id + '\'' +
+                ", kindFish=" + kindFish +
+                ", ageInMonths=" + ageInMonths +
+                ", position=" + position +
+                ", isProtected=" + isProtected +
+                '}';
+    }
+}
